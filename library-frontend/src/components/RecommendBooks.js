62f94849e879fcd68987a4360f9c13a704f5b250
dg0397 <rgg0397@gmail.com>
2021-08-14 18:58:41 -0400

    8.21 books by genre with GraphQL

--- library-frontend/src/components/RecommendBooks.js
+++ library-frontend/src/components/RecommendBooks.js
@@ -1,39 +1,38 @@
 import React, { useMemo } from 'react'
-import { useQuery } from '@apollo/client'
+import { useQuery,useLazyQuery } from '@apollo/client'
 import { USER_INFO, ALL_BOOKS } from '../queries'
 import BooksList from './BooksList'
 
 const Recommend = ({show}) => {
-    const resultUserInfo = useQuery(USER_INFO)
-    const resultAllBooks = useQuery(ALL_BOOKS)
-    const books = useMemo( ()=> {
+    const [getBooks,resultAllBooks] = useLazyQuery(ALL_BOOKS)
+    const resultUserInfo = useQuery(USER_INFO,{
+        onCompleted: ({me}) => getBooks({variables:{genre:me.favoriteGenre}})
+    })
+    const booksToShow = useMemo( ()=> {
         if(resultAllBooks.data){
           return [...resultAllBooks.data.allBooks]
         }
         return []
     },[resultAllBooks.data] )
-    console.log(resultUserInfo)
-    console.log(resultAllBooks)
-
+    
     const genreSelected = useMemo(()=>{
         if(resultUserInfo.data){
             return resultUserInfo.data.me.favoriteGenre
         }
         return ''
     },[resultUserInfo.data])
-
-    const generateSelectedBooks = ({books,genreSelected}) => {
-        if(genreSelected === 'all'){
-          return books
-        }
-        console.log(genreSelected)
-        const booksByGenre = books.filter( book => book.genres.includes(genreSelected) ) 
-        console.log(booksByGenre)
-        return  booksByGenre
-      }
-    
-    const booksToShow = useMemo(()=> generateSelectedBooks({books,genreSelected}),[genreSelected,books])
-    
+//
+    //const generateSelectedBooks = ({books,genreSelected}) => {
+    //    if(genreSelected === 'all'){
+    //      return books
+    //    }
+    //    console.log(genreSelected)
+    //    const booksByGenre = books.filter( book => book.genres.includes(genreSelected) ) 
+    //    console.log(booksByGenre)
+    //    return  booksByGenre
+    //  }
+    //
+    //const booksToShow = useMemo(()=> generateSelectedBooks({books,genreSelected}),[genreSelected,books])
 
     if(!show){
         return null
@@ -42,9 +41,6 @@ const Recommend = ({show}) => {
     if(resultAllBooks.loading){
         return <div>Loading...</div>
     }
-    console.log(books)
-    console.log(booksToShow)
-    console.log(genreSelected)
 
     return(
         <div>
